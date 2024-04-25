60dce9e3a467c46afb991bb4f16a17abdd234ec2
malithrashmika <malithrashmika10@gmail.com>
2024-04-25 16:41:45 +0530

    ui

--- src/main/java/lk/Ijse/Launcher.java
+++ src/main/java/lk/Ijse/Launcher.java
@@ -1,5 +1,19 @@
 package lk.Ijse;
 
-public class Launcher {
-    public
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("Login.fxml"))));
+stage.setTitle("Login");
+stage.show();
+    }
 }
